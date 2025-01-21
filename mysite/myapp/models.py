7a49bc1936c3f67940a6469f465c138587dadff5
Azizbek Rakhmanaliev <ligamentum88@gmail.com>
2025-01-21 12:23:32 +0500

    added image in models

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
   name = models.CharField(max_length=100, unique=True, verbose_name="Наименование")
   price = models.IntegerField(verbose_name="Цена")
   description = models.CharField(max_length=200, verbose_name="Описание")
+  image = models.ImageField(blank=True, upload_to='images', verbose_name="Изображение")
 
   objects = models.Manager()
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
